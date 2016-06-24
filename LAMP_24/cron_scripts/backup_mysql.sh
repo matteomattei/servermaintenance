9a726edb95d1e211608bf79ec0bb7d725535e24e
Matteo Mattei <matteo.mattei@gmail.com>
2016-06-24 17:38:17 +0200

    fix email send on mysql table corruption

--- LAMP_24/cron_scripts/backup_mysql.sh
+++ LAMP_24/cron_scripts/backup_mysql.sh
@@ -11,19 +11,23 @@ do
 	echo "processing ${db}"
 	[ -d "${BACKUP_FOLDER}/${db}" ] || mkdir -p "${BACKUP_FOLDER}/${db}"
 	cd "${BACKUP_FOLDER}/${db}"
-	mysqldump --opt -u${DB_USER} -p${DB_PASSWORD} "${db}" | gzip > dump_$(date +%F_%T).sql.gz || mail -s "error backing up database ${db}" ${MAIL_NOTIFICATION}
-	while [ `ls dump_* | wc -l` -gt ${NUM_COPIES} ]
+	NOW=$(date +%F_%T)
+	mysqldump --opt -u${DB_USER} -p${DB_PASSWORD} "${db}" > dump_${NOW}.sql || mail -s "error backing up database ${db}" ${MAIL_NOTIFICATION}
+	gzip dump_${NOW}.sql || mail -s "error compressing database ${db}" ${MAIL_NOTIFICATION}
+	while [ `ls dump_*.gz | wc -l` -gt ${NUM_COPIES} ]
 	do
-		FIRST=`ls -t dump_* | tail -n1`
+		FIRST=`ls -t dump_*.gz | tail -n1`
 		rm -f "${FIRST}"
 	done
 done
 
 mkdir -p ${BACKUP_FOLDER}/ALL_DATABASES
 cd ${BACKUP_FOLDER}/ALL_DATABASES
-mysqldump --opt -u${DB_USER} -p${DB_PASSWORD} --events --ignore-table=mysql.event --all-databases | gzip > all_$(date +%F_%T).sql.gz || mail -s "error backing up all databases" ${MAIL_NOTIFICATION}
-while [ `ls all_* | wc -l` -gt ${NUM_COPIES} ]
+NOW=$(date +%F_%T)
+mysqldump --opt -u${DB_USER} -p${DB_PASSWORD} --events --ignore-table=mysql.event --all-databases > all_${NOW}.sql || mail -s "error backing up all databases" ${MAIL_NOTIFICATION}
+gzip all_${NOW}.sql || mail -s "error compressing all databases" ${MAIL_NOTIFICATION}
+while [ `ls all_*.gz | wc -l` -gt ${NUM_COPIES} ]
 do
-	FIRST=`ls -t all_* | tail -n1`
+	FIRST=`ls -t all_*.gz | tail -n1`
 	rm -f "${FIRST}"
 done
